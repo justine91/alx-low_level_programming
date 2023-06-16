36ff89f9334676ce1714dfc60f89e388a850de4d
justine91 <ihezuejustine.c@gmail.com>
2023-06-16 09:42:44 -0700

    C - Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
-  * main - entry point
-  * return : always 0
-*/
+  *main - Entry point
+  *Return: always 0 (success)
+  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
