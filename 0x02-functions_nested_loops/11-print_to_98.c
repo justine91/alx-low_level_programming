9ba283e0e2405323fd650c254f1e633e393c56e9
justine91 <ihezuejustine.c@gmail.com>
2023-06-21 15:08:16 -0700

    Functions_nested_loops update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -35,12 +35,12 @@ void print_to_98(int n)
 			if (n == 98)
 			{
 				printf("%d", n);
-				printf("\n');
+				printf("\n");
 				break;
 			}
 			else
 			{
-				printf("'%'d, ", n);
+				printf("%d, ", n);
 			}
 		}
 	}
